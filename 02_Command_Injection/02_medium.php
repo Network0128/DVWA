13e4ae2b5d988cd2acb4a845b3a079e0a222e8d0
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-03-01 15:33:48 +0900

    Update 02_medium.php

--- 02_Command_Injection/02_medium.php
+++ 02_Command_Injection/02_medium.php
@@ -1,34 +1,37 @@
 이 스크립트는 웹 양식을 통해 사용자 입력을 받아 해당 IP 주소로 ping 테스트를 수행하고 결과를 웹 페이지에 출력합니다. 
 보안상의 이유로, 입력된 값에서 특정 문자(&&, ;)를 제거하는 단계를 포함하고 있습니다. 
 이는 주입 공격(injection attacks)을 방지하기 위한 조치입니다.
+개선점 : shell_exec 함수 대신 exec 함수를 사용하여 더 안전하게 명령어를 실행할 수 있습니다.
 
 <?php
 
-if( isset( $_POST[ 'Submit' ]  ) ) { // 'Submit' 버튼이 눌렸는지 확인
-    // 입력 받기
-    $target = $_REQUEST[ 'ip' ]; // 사용자로부터 입력 받은 'ip' 값을 변수에 저장
+// POST 방식으로 'Submit'이라는 이름의 버튼이 클릭되었는지 확인합니다.
+if( isset( $_POST[ 'Submit' ] ) ) {
+  // 사용자 입력을 받습니다.
+  $target = $_REQUEST[ 'ip' ]; // 사용자로부터 입력 받은 'ip' 값을 변수에 저장
 
-    // 블랙리스트 설정
-    $substitutions = array(
-        '&&' => '', // '&&' 문자 제거
-        ';'  => '', // ';' 문자 제거
-    );
+  // 금지된 문자를 제거하기 위한 배열을 설정합니다. 이 배열은 제거해야 할 문자나 문자열을 지정합니다.
+  $substitutions = array(
+    '&&' => '', // &&를 공백으로 바꿉니다.
+    ';' => '', // ;를 공백으로 바꿉니다.
+  );
 
-    // 배열(블랙리스트)에 있는 문자들을 제거
-    $target = str_replace( array_keys( $substitutions ), $substitutions, $target );
 
-    // 운영 체제 확인 후 ping 명령 실행
-    if( stristr( php_uname( 's' ), 'Windows NT' ) ) { // 현재 서버의 운영 체제가 Windows NT인지 확인
-        // Windows 용 명령
-        $cmd = shell_exec( 'ping  ' . $target ); // Windows에서의 ping 명령 실행
-    }
-    else {
-        // *nix (Linux나 Unix 같은) 용 명령
-        $cmd = shell_exec( 'ping  -c 4 ' . $target ); // *nix에서의 ping 명령 실행, '-c 4'는 4번 핑을 의미
-    }
+  // // 금지된 문자를 제거합니다. 배열의 키(블랙리스트(금지된 문자)에 지정된 문자나 문자열)를 검색하여, 해당하는 모든 문자나 문자열을 제거합니다.
+  $target = str_replace( array_keys( $substitutions ), $substitutions, $target );
 
-    // 최종 사용자에게 결과 피드백
-    echo "<pre>{$cmd}</pre>"; // 명령의 실행 결과를 보기 좋게 표시
+  // 운영체제를 확인하고 해당하는 `ping` 명령어를 실행합니다.
+  if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
+    // Windows 기반 운영체제의 경우
+    $cmd = shell_exec( 'ping ' . $target );
+  }
+  else {
+    // Windows 이외의 *nix 기반 운영체제의 경우
+    $cmd = shell_exec( 'ping -c 4 ' . $target );
+  }
+
+  // 최종 사용자에게 실행 결과를 보여줍니다.
+  echo "<pre>{$cmd}</pre>"; // 명령의 실행 결과를 보기 좋게 표시
 }
 
 ?>
